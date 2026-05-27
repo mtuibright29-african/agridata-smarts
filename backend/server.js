357bc7a532d4b3b1ccbd4dc51848c93631f3bc73
benjamini-msafiri <benjaminimsafiri07@gmail.com>
2026-05-27 15:48:50 +0300

    Fix Express OPTIONS wildcard route for Render deploy

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*foo', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
